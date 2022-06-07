7fadc56b7faadbf56d3c3f32df47f6266528fb41
Shiba Prasad Kar <Shibaprasadkar50@gmail.com>
2022-06-07 09:32:07 +0530

    Create cleaner.py

---
+++ cleaner.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+directory = "C:\\Users\\shiva\\OneDrive\\Programming" # Change this to your directory
+deleteDirectory = "node_modules" # Directory to be deleted #build
+for root, subdirectories, files in os.walk(directory):
+    for subdirectory in subdirectories:
+        if(subdirectory == "node_modules"):
+            print(f'Deleting {os.path.join(root, subdirectory)}')
+            shutil.rmtree(os.path.join(root, subdirectory),ignore_errors=True)
+           # print("Build directory deleted!")
